b2f633a02d71b3b8ef7486e48087e42876a3a3ec
Arnold <luizarnolch@gmail.com>
2021-04-25 11:59:39 -0500

    mi primer commit

---
+++ src/keys.js
@@ -0,0 +1,10 @@
+//la configuración de la base de datos
+
+module.exports = {
+    database: {
+        host: 'localhost',
+        user: 'root',
+        password: 'FisiCalidad',
+        database: 'petswork3'
+    }
+};

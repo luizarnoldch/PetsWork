3e2e15096baf5e89a5448a042a582464c218ba56
arnold <luizarnolch@gmai.com>
2021-04-25 19:05:41 -0500

    mysql-heroku2

--- src/keys.js
+++ src/keys.js
@@ -3,8 +3,9 @@
 module.exports = {
     database: {
         host: 'us-cdbr-east-03.cleardb.com',
-        user: 'b6aa452a6aeaca',
-        password: '7f8bcc9e',
-        database: 'heroku_70488250b2e2d70'
+        user: 'bb0956035bbb3e',
+        password: 'be51e660',
+        database: 'heroku_8ef9b1179b3c7f3'
     }
 };
+//mysql://bb0956035bbb3e:be51e660@us-cdbr-east-03.cleardb.com/heroku_8ef9b1179b3c7f3?reconnect=true
